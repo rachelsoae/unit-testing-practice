69cecb81e4b2377129b3bbe4049fc0290838d331
Rachel Prather <rachelprather1@gmail.com>
2023-04-04 20:42:38 -0700

    Complete practice tests

--- practice.js
+++ practice.js
@@ -1,10 +1,36 @@
-function findRemainder() {
+function findRemainder(num1, num2) {
+  return num1 % num2;
+}
 
+function startGPS(pointA, pointB) {
+  if (pointA === pointB) {
+    return `You are already at the ${pointA}!  Would you like to go somewhere else?`;
+  } else {
+    return `Taking you from ${pointA} to ${pointB}!  Getting directions now!`;
+  }
+}
+
+function createUser(firstName, lastName, birthDate, onlineStatus = false) {
+  return user = {
+    firstName: firstName,
+    lastName: lastName,
+    birthDate: birthDate,
+    onlineStatus: onlineStatus
+  }
+}
+
+function makePizza(pizza, toppings) {
+  pizza.cooked = true;
+  if (pizza.toppings.length < 5) {
+    pizza.toppings.push(toppings);
+  }
+  
+  return pizza
 }
 
 module.exports = {
   findRemainder,
-  // startGPS,
-  // createUser,
-  // makePizza
+  startGPS,
+  createUser,
+  makePizza
 }
